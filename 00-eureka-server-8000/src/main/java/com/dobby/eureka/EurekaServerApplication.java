8c52768cfb06fd4bbd97ffcb90a5e42c49e3a672
liguoqing <lgq0218@gmail.com>
2020-04-05 01:33:56 +0800

    添加一个Eureka服务端作为注册中心

---
+++ 00-eureka-server-8000/src/main/java/com/dobby/eureka/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.dobby.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @EnableEurekaServer:开启Eureka服务
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
