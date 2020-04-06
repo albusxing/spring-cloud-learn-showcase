8a36872f1cc17af76e52d23fc22edb59467afb6f
liguoqing <lgq0218@gmail.com>
2020-04-06 16:03:52 +0800

    添加 Spring Cloud Config 配置中心

---
+++ 00-config-server-9999/src/main/java/com/dobby/config/ConfigServerApplication.java
@@ -0,0 +1,18 @@
+package com.dobby.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @EnableConfigServer ： 开启配置服务功能
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
---
+++ 00-config-server-9999/src/test/java/com/dobby/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dobby.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
